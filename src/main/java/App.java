0c428790ba91aa5227c4e5645ea076895d827500
basniniv <155753031+basniniv@users.noreply.github.com>
2024-04-07 15:56:49 +0400

    Создал класс Cat, реализовал зависимости

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,27 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
+
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
         System.out.println(bean.getMessage());
+        System.out.println(bean1.getMessage());
+        System.out.println(bean == bean1);
+
+        Cat catBean1 = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println(catBean1.getName());
+        System.out.println(catBean1.getName());
+
+        System.out.println(catBean1 == catBean2);
+
+
     }
 }
